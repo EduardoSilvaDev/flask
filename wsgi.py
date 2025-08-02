4dac7f12cad8b772f047b5cbad7013918b98059b
EduardoSilvaDev <carlos.duhduh5569@gmail.com>
2025-08-01 22:21:23 -0300

    acrescentar algumas informacoes

--- wsgi.py
+++ wsgi.py
@@ -1,3 +1,6 @@
 from estudo import app
 
 # nao precisaria fazer isso se o Módulo fosse nomeado de 'app', só que como é nomedo de 'estudo', é necessario fazer isso
+
+
+# depois disto configurado usa o comando 'flask db init' no terminal para que o banco de dados seja criado, utiliza apenas uma unica vez, é criado as migrations
