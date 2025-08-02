cc2a633dff1ec69300db1b14b1fb8f9ec13d52fe
EduardoSilvaDev <carlos.duhduh5569@gmail.com>
2025-08-01 23:25:30 -0300

    formularios GET e POST salvando dados no db

--- estudo/views.py
+++ estudo/views.py
@@ -1,12 +1,13 @@
-from estudo import app
-from flask import render_template, url_for
+from estudo import app, db
+from flask import render_template, url_for, request
+from estudo.models import Contato
 
 @app.route('/')
 def homepage():
     context = {
         'name': 'Eduardo',
         'idade': 30,
-        'cidade': 'São Paulo'
+        'cidade': 'Caruaru'
     }
     return render_template('index.html',context=context)
 
@@ -14,3 +15,27 @@ def homepage():
 @app.route('/new/')
 def newpage():
     return render_template('newpage.html')
+
+@app.route('/contato/', methods=['GET','POST'])
+def contato():
+    context = {}
+    if request.method == 'GET':
+        pesquisa = request.args.get('pesquisa')
+        print('GET',pesquisa)
+        context.update({'pesquisa':pesquisa})
+    if request.method == 'POST':
+        nome = request.form['name']
+        email = request.form['email']
+        assunto = request.form['subject']
+        mensagem = request.form['message']
+        
+        contato = Contato(
+            name=nome,
+            email=email,
+            subject=assunto,
+            message=mensagem
+        )
+        db.session.add(contato)
+        db.session.commit()
+        
+    return render_template('contato.html',context=context)
