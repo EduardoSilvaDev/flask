9ad0faca78f4d1ef69326f42a3a0b02fb91dde25
EduardoSilvaDev <carlos.duhduh5569@gmail.com>
2025-08-01 22:30:02 -0300

    coluna response adicionado na tabela contato

--- estudo/models.py
+++ estudo/models.py
@@ -7,6 +7,7 @@ class Contato(db.Model):
     email = db.Column(db.String, nullable = True)
     subject = db.Column(db.String, nullable = True)
     message = db.Column(db.String, nullable = True)
+    response = db.Column(db.Integer, default = 0)
     
     dt_created = db.Column(db.DateTime, default = lambda: datetime.now(timezone.utc))
     active = db.Column(db.Boolean, default = True)
