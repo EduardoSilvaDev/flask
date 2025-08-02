37d2aa13f8c5087cb8ab3d5eeda3a1408b12ca17
EduardoSilvaDev <carlos.duhduh5569@gmail.com>
2025-08-01 22:05:21 -0300

    criacao do banco de dados sqlalchemy e migrates

--- estudo/__init__.py
+++ estudo/__init__.py
@@ -1,6 +1,13 @@
 from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
+from flask_migrate import Migrate
 
 app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///database.db'
+app.config['SQLACHEMY_TRACK_MODIFICATIONS'] = False
+
+db = SQLAlchemy(app=app)
+migrate = Migrate(app=app, db=db)
 
 from estudo.views import homepage
 # https://github.com/EduardoSilvaDev/flask.git
